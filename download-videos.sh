f0bfb72aa168131b6e431277a84d223ccdb9cab2
Felix Niemeyer <niemeyer.felix@gmail.com>
2026-05-15 14:04:57 +0200

    download videos in lq

--- download-videos.sh
+++ download-videos.sh
@@ -1,2 +1,3 @@
+mkdir video-downloads -p
 cd video-downloads
-yt-dlp --playlist-random --write-info-json 'https://www.pornhub.com/pornstar/janice-griffith'
+yt-dlp --playlist-random --write-info-json 'https://www.pornhub.com/pornstar/janice-griffith' -S "height:480"
